490ad3a8e78b2f2d057de7a654f0aa5e64f3313c
kanemototomoki <kanemototomoki@gmail.com>
2023-11-19 15:03:11 +0900

    update: biome

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,27 +1,29 @@
-import type { FC } from "hono/jsx";
+import type { Child, FC } from "hono/jsx";
 
-type Props = {};
+type Props = {
+	children?: Child;
+};
 const Layout: FC<Props> = (props) => {
-  return (
-    <html>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>my-rest-api</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>{props.children}</body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>my-rest-api</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossorigin=""
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body>{props.children}</body>
+		</html>
+	);
 };
 
 export default Layout;
--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -4,25 +4,25 @@ import Layout from "../Layout";
 import type { Route } from "../types";
 
 type Props = {
-  routes: Route[];
+	routes: Route[];
 };
 
 const Top: FC<Props> = ({ routes }) => {
-  return (
-    <Layout>
-      <h1>ルート一覧</h1>
-      <ul>
-        {routes.map(({ path, description }) => {
-          return (
-            <li key={path}>
-              <code>path: {path}</code>
-              <p>desc: {description}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<h1>ルート一覧</h1>
+			<ul>
+				{routes.map(({ path, description }) => {
+					return (
+						<li key={path}>
+							<code>path: {path}</code>
+							<p>desc: {description}</p>
+						</li>
+					);
+				})}
+			</ul>
+		</Layout>
+	);
 };
 
 export default Top;
